b8a2fa1b6b02683e8b5e85a4bec77adbc4d28300
osama7star <osama.alnagem@gmail.com>
2021-09-22 20:46:41 +0300

    - improving search page

--- lib/screens/other_pages/search/search.dart
+++ lib/screens/other_pages/search/search.dart
@@ -14,69 +14,91 @@ import 'package:getir_clone/utilities/sizes.dart';
 class Search extends StatefulWidget {
   const Search({Key? key}) : super(key: key);
 
-
-
   @override
   State<Search> createState() => _SearchState();
 }
 
 class _SearchState extends State<Search> {
   bool showData = false;
+  TextEditingController controller = new TextEditingController();
+  IconData passwordSuffixIcon = Icons.mic;
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
-        resizeToAvoidBottomInset:false,
+        resizeToAvoidBottomInset: false,
         appBar: CustomAppBar(
             context: context,
             text: 'Search',
             color: CustomColors.white,
             fontSize: CustomSizes.header4),
         body: SizedBox(
-          height:getScreenHeight()-150,
+          height: getScreenHeight() - 150,
           child: ListView(children: [
-
             ///TODO: SEND DATA FROM SON TO PARENT
-            InputFieldW(showData:showData),
-            showData?Column(
-              crossAxisAlignment:CrossAxisAlignment.start,
-              children:[
-                Padding(
-                  padding:  EdgeInsets.all(CustomSizes.padding5),
-                  child: CustomText(
-                      text: 'Arama Sonucu',
-                      fontSize: CustomSizes.header5,
-                      color: CustomColors.black.withOpacity(0.5),
-                      isCenter: false),
-                ),
-
-                Card(
-                  margin:EdgeInsets.zero,
-
-                  child: Container(
-                    padding:
-                    EdgeInsets.symmetric(horizontal: CustomSizes.padding8 / 1.5),
-                    child: GridView.builder(
-                      shrinkWrap: true,
-                      itemCount: categoryList.length,
-                      primary: false,
-                      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                        crossAxisCount: 3,
-                        crossAxisSpacing: 8,
-                        mainAxisSpacing: 8,
-                        childAspectRatio: 3 / 6,
-                      ),
-                      itemBuilder: (context, index) {
-                        return productWidget(product: productList[index],);
-                      },
-                    ),
-                  ),
-                ),
-              ]
-            ):Container(width:0),
+            Card(
+                margin: EdgeInsets.zero,
+                child: InputFieldW(
+                  showData: showData,
+                  controller: controller,
+                  function: () {
+                    if (controller.text.isNotEmpty) {
+                      setState(() {
+                        showData = true;
+                        passwordSuffixIcon = Icons.close;
+                      });
+                    }
+                  },
+                  function1: () {
+                    controller.text = '';
+                    setState(() {
+                      showData = false;
+                      passwordSuffixIcon = Icons.mic;
+                    });
+                  },
+                  passwordSuffixIcon: passwordSuffixIcon,
+                )),
+            showData
+                ? Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                        Padding(
+                          padding: EdgeInsets.all(CustomSizes.padding5),
+                          child: CustomText(
+                              text: 'Arama Sonucu',
+                              fontSize: CustomSizes.header5,
+                              color: CustomColors.black.withOpacity(0.5),
+                              isCenter: false),
+                        ),
+                        Card(
+                          margin: EdgeInsets.zero,
+                          child: Container(
+                            padding: EdgeInsets.symmetric(
+                                horizontal: CustomSizes.padding8 / 1.5),
+                            child: GridView.builder(
+                              shrinkWrap: true,
+                              itemCount: productList.length,
+                              primary: false,
+                              gridDelegate:
+                                  const SliverGridDelegateWithFixedCrossAxisCount(
+                                crossAxisCount: 3,
+                                crossAxisSpacing: 8,
+                                mainAxisSpacing: 8,
+                                childAspectRatio: 3 / 6.2,
+                              ),
+                              itemBuilder: (context, index) {
+                                return productWidget(
+                                  product: productList[index],
+                                );
+                              },
+                            ),
+                          ),
+                        ),
+                      ])
+                : Container(width: 0),
           ]),
         ),
-     bottomNavigationBar:  CustomBottomNavBar(selectedMenu: MenuState.search));
+        bottomNavigationBar:
+            CustomBottomNavBar(selectedMenu: MenuState.search));
   }
 }
--- lib/screens/other_pages/search/widgets/widgets.dart
+++ lib/screens/other_pages/search/widgets/widgets.dart
@@ -6,37 +6,33 @@ import 'package:getir_clone/utilities/colors.dart';
 import 'package:getir_clone/utilities/sizes.dart';
 
 class InputFieldW extends StatefulWidget {
-   InputFieldW({Key? key, this.showData = false}) : super(key: key);
+   InputFieldW({Key? key, this.showData = false,required this.function,required this.function1,required this.controller,this.passwordSuffixIcon=Icons.mic}) : super(key: key);
     bool showData;
-  @override
+    Function() function ;
+    Function() function1 ;
+
+   TextEditingController controller ;
+   IconData passwordSuffixIcon ;
+
+   @override
   State<InputFieldW> createState() => _InputField1State();
 }
 
 class _InputField1State extends State<InputFieldW> {
-  TextEditingController controller = new TextEditingController();
-  IconData passwordSuffixIcon = Icons.mic;
   @override
   Widget build(BuildContext context) {
     return               InputField(
-      controller: controller,
-      hintText: 'Search Address',
+      controller: widget.controller,
+      hintText: 'Search Products',
       textInputType: TextInputType.text,
       fontSize: CustomSizes.header4,
       suffixIcon: IconButton(
         icon: Icon(
-          passwordSuffixIcon,
+        widget.  passwordSuffixIcon,
           size: CustomSizes.iconSize,
           color: CustomColors.black,
         ),
-        onPressed: () {
-          log(controller.text);
-          controller.text = '';
-          setState((){
-            widget.showData = false;
-            passwordSuffixIcon = Icons.mic;
-
-          });
-        },
+        onPressed:widget. function1,
       ),
       prefixIcon: IconButton(
         icon: Icon(
@@ -44,28 +40,17 @@ class _InputField1State extends State<InputFieldW> {
           size: CustomSizes.iconSizeMedium,
           color: CustomColors.primary,
         ),
-        onPressed: () {
-          passwordSuffixIcon = Icons.mic;
-
-          if (controller.text.length>0)
-          {
-            setState((){
-              widget.showData = true;
-              passwordSuffixIcon =Icons.close;
-
-            });
-          }
-        },
+        onPressed: widget.function,
       ),
       onChanged: (name){
 
         setState(() {
-          if(controller.text.length>0)
+          if(widget.controller.text.isNotEmpty)
           {
-            passwordSuffixIcon =Icons.close;
+            widget.passwordSuffixIcon =Icons.close;
           }
           else{
-            passwordSuffixIcon = Icons.mic;
+            widget.passwordSuffixIcon = Icons.mic;
 
           }
         });
